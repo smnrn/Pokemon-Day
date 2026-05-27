41498f2904414b2bc5c0a12733f0c06e41793cbe
smnrn <simonronjoshua@gmail.com>
2026-05-28 03:59:54 +0800

    Prioritize Gen 5 animated sprites to fix off-center 3D model rips like Mewtwo

--- src/sections/PokemonProfile.jsx
+++ src/sections/PokemonProfile.jsx
@@ -141,8 +141,8 @@ export default function PokemonProfile() {
 
   const spriteUrl = pokemon
     ? (shiny
-        ? (pokemon.sprites.other?.showdown?.front_shiny || pokemon.sprites.versions?.['generation-v']?.['black-white']?.animated?.front_shiny || pokemon.sprites.other?.['official-artwork']?.front_shiny || pokemon.sprites.front_shiny)
-        : (pokemon.sprites.other?.showdown?.front_default || pokemon.sprites.versions?.['generation-v']?.['black-white']?.animated?.front_default || pokemon.sprites.other?.['official-artwork']?.front_default || pokemon.sprites.front_default))
+        ? (pokemon.sprites.versions?.['generation-v']?.['black-white']?.animated?.front_shiny || pokemon.sprites.other?.showdown?.front_shiny || pokemon.sprites.other?.['official-artwork']?.front_shiny || pokemon.sprites.front_shiny)
+        : (pokemon.sprites.versions?.['generation-v']?.['black-white']?.animated?.front_default || pokemon.sprites.other?.showdown?.front_default || pokemon.sprites.other?.['official-artwork']?.front_default || pokemon.sprites.front_default))
     : null;
 
   const flavorText = species?.flavor_text_entries?.find(e => e.language.name === 'en')?.flavor_text?.replace(/\f/g, ' ') || '';
